db5aae0bf8081e9fa5d518c4bbc92f36b521792b
Ryan Williams <ryan.blake.williams@gmail.com>
2015-03-28 19:53:06 +0000

    add draggable ground/plateau

--- static/js/trajectories.js
+++ static/js/trajectories.js
@@ -1,5 +1,9 @@
 /** @jsx React.DOM */
 
+function path() {
+  return "M" + Array.prototype.slice.call(arguments).map(function(p) { return p.join(','); }).join(' L');
+}
+
 var Circle = React.createClass({
   getInitialState: function() {
     return {};
@@ -39,26 +43,87 @@ var Circle = React.createClass({
 
 var Parabola = React.createClass({
   render: function() {
+    var ps = this.props.ps;
+    var midPoint = [ (ps[0][0] + ps[2][0]) / 2, (ps[0][1] + ps[2][1]) / 2 ];
+    var controlPoint = [ 2*ps[1][0] - midPoint[0], 2*ps[1][1] - midPoint[1] ]
     var pathStr = [
       "M" + this.props.ps[0].join(','),
-      "Q" + this.props.ps[1].join(','),
+      "Q" + controlPoint.join(','),
       this.props.ps[2].join(',')
-    ].join(' ');
-    var lineStr = [
-      "M" + this.props.ps[0].join(','),
-      "L" + this.props.ps[2].join(',')
     ].join(' ');
     return <g>
       <path d={pathStr}/>
-      <path d={lineStr}/>
+      <path d={path(this.props.ps[0], this.props.ps[2])}/>
     </g>;
+  }
+});
+
+var YLine = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  handleStart: function (event, ui) {
+    if (!this.state.dragStartX) {
+      console.log("storing drag start x: %d", this.props.base[0]);
+      this.state.dragStartX = this.props.x;
+    }
+  },
+  handleDrag: function (event, ui) {
+    //console.log("updating x, original: %d, new: %d", this.state.dragStartX, this.state.dragStartX + ui.position.left);
+    this.props.updateX(this.state.dragStartX + ui.position.left);
+  },
+  render: function() {
+    return (
+      <ReactDraggable
+            axis="x"
+            onStart={this.handleStart}
+            onDrag={this.handleDrag}
+      >
+        <g className="yline">
+          <path d={path(this.props.base, [ this.props.base[0], this.props.base[1] - this.props.length ])} ></path>
+          <rect width={20} height={this.props.length} x={this.props.base[0]-10} y={this.props.base[1] - this.props.length} />
+        </g>
+      </ReactDraggable>
+    );
+  }
+});
+
+var PlateauLine = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  handleStart: function (event, ui) {
+    if (!this.state.dragStartY) {
+      console.log("storing drag start y: %d", this.props.base[0]);
+      this.state.dragStartY = this.props.y;
+    }
+  },
+  handleDrag: function (event, ui) {
+    //console.log("updating x, original: %d, new: %d", this.state.dragStartX, this.state.dragStartX + ui.position.left);
+    this.props.updateY(this.state.dragStartY - ui.position.top);
+  },
+  render: function() {
+    return (
+          <ReactDraggable
+                axis="y"
+                onStart={this.handleStart}
+                onDrag={this.handleDrag}
+          >
+            <g className="plateau-line">
+              <path d={path(this.props.base, [ this.props.base[0] + this.props.length, this.props.base[1] ])} />
+              <rect height={20} width={this.props.length} x={this.props.base[0]} y={this.props.base[1] - 10} />
+            </g>
+          </ReactDraggable>
+    );
   }
 });
 
 var Plot = React.createClass({
   getInitialState: function() {
     return {
-      points: [[10,200], [50,10], [75,50]]
+      points: [[10,200], [50,10], [75,50]],
+      x: 100,
+      y: 50
     }
   },
   updatePoint: function(idx) {
@@ -70,14 +135,27 @@ var Plot = React.createClass({
       this.setState(state);
     }.bind(this);
   },
+  updateX: function(x) {
+    this.setState({ x: x });
+  },
+  updateY: function(y) { this.setState({ y: y }); },
   render: function() {
     var updatePoint = this.updatePoint;
     var circles = this.state.points.map(function(point, idx) {
       return <Circle p={point} onMove={updatePoint(idx)} />
     });
+
+    var firstPoint = this.state.points[0];
+    var yBasePoint = [ firstPoint[0] + this.state.x, firstPoint[1] ];
+    var yTopPoint = [ yBasePoint[0], yBasePoint[1] - this.state.y ];
+    //var yEndPoint = [ yTopPoint[0] + 500 , yTopPoint[1] ];
+
     return <svg className="plot">
       <Parabola ps={this.state.points}/>
       {circles}
+      <path d={path(firstPoint, yBasePoint)} />
+      <YLine base={yBasePoint} length={this.state.y} x={this.state.x} updateX={this.updateX} />
+      <PlateauLine base={yTopPoint} length={500} y={this.state.y} updateY={this.updateY} />
     </svg>;
   }
 });
